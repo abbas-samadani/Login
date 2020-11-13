88f4f66420f741140f9b794d17be4b3592449b24
unknown <r.moosavi820@gamil.com>
2020-11-13 16:46:05 +0330

    sending token to server with axios and get information to server

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Login from './Pages/Login'
+import Dashboard from './Pages/Dashboard'
+import { useAuthState } from './Content/auth-content'
 export default function App() {
+    const {token} = useAuthState()    
     return (
-        <Login />
+        <>
+            {token ? <Dashboard /> : <Login />}
+        </>
     )
 }
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,12 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useAuthDispach } from '../../Content/auth-content'
 import './style.css'
+import axios from 'axios'
+import { actionType } from '../../Content/reducer';
+
+
 export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [token, setToken] = useState('');
+
+    const dispatch = useAuthDispach()
+    const fetchToken = async (username, password) => {
+        return axios.post('http://localhost:3001/login', {
+            username,
+            password
+        }).then(response => response.data)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetchToken(username, password)
+            .then(({ data, success }) => {
+                if (success) {
+                    setToken(data)
+                }
+            }
+            )
+    }
+
+    const fetchCurrentUserInfo = (token) => {
+        return axios.get('http://localhost:3001/users/me', {
+            headers: {
+                authorization: token
+            }
+        }).then(response => response.data)
+    }
+
+    useEffect(() => {
+        fetchCurrentUserInfo(token)
+            .then(({ success, data }) => {
+                if (success) {
+                    dispatch({
+                        type: actionType.LOGIN_SUCCESS,
+                        payload: {
+                            user: data,
+                            token
+                        }
+                    })
+                }
+            }
+            )
+    }, [token, dispatch])
+
+
     return (
         <div className="login">
             <h1>Login</h1>
-            <form method="post" >
-                <input type="text" placeholder="Username" required="required" />
-                <input type="password" placeholder="Password" required="required" />
+            <form method="post" onSubmit={handleSubmit}>
+                <input type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Username"
+                    required="required" />
+                <input type="password"
+                    value={password} onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    required="required" />
                 <button type="submit" className="btn btn-primary btn-block btn-large">Let me in.</button>
             </form>
         </div>
